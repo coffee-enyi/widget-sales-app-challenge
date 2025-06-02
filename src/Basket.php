22022e4f9e52138c5db3283a861d1e771f3de4f0
Enyinna <enyoibe@gmail.com>
2025-06-02 05:32:52 +0100

    write initial basket code

---
+++ src/Basket.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App;
+
+use App\Delivery\DeliveryStrategyInterface;
+use App\Offer\OfferStrategyInterface;
+use Brick\Money\Money;
+
+class Basket
+{
+    private array $items = [];
+
+    public function __construct(
+        private array $catalogue,
+        private DeliveryStrategyInterface $delivery,
+        private OfferStrategyInterface $offer
+        ) {}
+
+    public function add(string $code): void
+    {
+        if(isset($this->catalogue[$code]))
+        {
+            $this->items[] = $this->catalogue[$code];
+        }
+    }
+
+    public function total(): Money
+    {
+        $products = $this->offer->apply($this->items);
+        
+        $subtotal = Money::of(0, '$');
+        foreach($products as $p)
+        {
+            $subtotal = $subtotal->plus($product->price);
+        }
+
+        $deliveryFee = $this->delivery->getDeliveryFee($subtotal);
+        
+        return $subtotal->plus($deliveryFee);
+    }
+}
--- src/Delivery/DeliveryStrategy.php
+++ src/Delivery/DeliveryStrategy.php
@@ -9,11 +9,11 @@ class DeliveryStrategy implements DeliveryStrategyInterface
 {
     public function getDeliveryFee(Money $subtotal): Money
     {
-        if ($subtotal->isLessThan(Money::of(50, 'USD'))) {
+        if ($subtotal->isLessThan(Money::of(50, '$'))) {
             return Money::of(4.95, '$');
         }
 
-        if ($subtotal->isLessThan(Money::of(90, 'USD'))) {
+        if ($subtotal->isLessThan(Money::of(90, '$'))) {
             return Money::of(2.95, '$');
         }
 
