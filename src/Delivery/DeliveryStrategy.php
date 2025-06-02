adcf8476b01d04fdc663bcfa9ccd57a1c1b568f9
Enyinna <enyoibe@gmail.com>
2025-06-02 05:18:28 +0100

    create the required strategies

---
+++ src/Delivery/DeliveryStrategy.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Delivery;
+
+use App\Delivery\DeliveryStrategyInterface;
+use Brick\Money\Money;
+
+class DeliveryStrategy implements DeliveryStrategyInterface
+{
+    public function getDeliveryFee(Money $subtotal): Money
+    {
+        if ($subtotal->isLessThan(Money::of(50, 'USD'))) {
+            return Money::of(4.95, '$');
+        }
+
+        if ($subtotal->isLessThan(Money::of(90, 'USD'))) {
+            return Money::of(2.95, '$');
+        }
+
+        return Money::of(0, '$');
+    }
+}
